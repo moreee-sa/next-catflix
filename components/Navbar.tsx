9b0071ccbdad1bb76d4943a27bfccd1f6e62a534
moreee-sa <morenolms41@gmail.com>
2025-06-18 21:08:25 +0200

    Aggiornato codice per il corretto funzionamento con styled-components

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import styled from "styled-components"
+const mobileBreakpoint = '767px'
+
+const Title = styled.h1`
+  font-size: 25px;
+  color: red;
+`;
+
+export default function Navbar() {
+  return (
+    <div>
+    </div>
+  )
+}
