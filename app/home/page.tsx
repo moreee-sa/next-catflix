75ec3a916a7c0cf808c7c00e4a03af022d362e82
moreee-sa <morenolms41@gmail.com>
2025-06-18 22:18:58 +0200

    Aggiunte le card

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,25 @@
 export default async function Page() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  return <h1>Hello Next.js!</h1>
+  await new Promise(resolve => setTimeout(resolve, 2500));
+  
+  const n: number = 50;
+
+  return (
+    <div
+      className="grid gap-4 p-5 justify-center"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+        maxWidth: "100%",
+        margin: "0 auto",
+      }}
+    >
+      {Array.from({ length: n }).map((_, i) => (
+        <div
+          key={i}
+          className="bg-amber-100 rounded-2xl"
+          style={{ height: "300px", width: "100%" }}
+        ></div>
+      ))}
+    </div>
+  );
 }
