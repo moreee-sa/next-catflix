ecc8050a5f24ff0268f1751dcaaf59f48e2e9e19
moreee-sa <morenolms41@gmail.com>
2025-06-18 22:55:56 +0200

    Aggiunta la pagina not-found.tsx per evitare errori durante il fetch

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 type Film = {
   _id: string;
@@ -9,41 +10,50 @@ type Film = {
   poster_path: string;
 };
 
-export default async function Page() {
+export default async function HomePage() {
   await new Promise(resolve => setTimeout(resolve, 1500));
 
-  const res = await fetch("http://192.168.1.221:8000/movies", {
-    cache: "no-store", // evita caching in fase di sviluppo
-  });
+  try {
+    const res = await fetch("http://192.168.1.221:8000/movies", {
+      cache: "no-store",
+    });
 
-  const data = await res.json();
-  const movies: Film[] = data.Film.Film;
+    if (!res.ok) {
+      notFound();
+    }
 
-  return (
-    <div
-      className="grid gap-4 p-5 justify-center"
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-        maxWidth: "100%",
-        margin: "0 auto",
-      }}
-    >
-      {movies.map((movie) => (
-        <Link key={movie._id} href={`/movie/${movie.id_tmdb}`}>
-          <div
-            className="rounded-2xl relative"
-            style={{ height: "300px", width: "200px" }}
-          >
-            <Image
-              src={`http://192.168.1.221:8000/poster/${movie.poster_path}`}
-              alt={movie.title}
-              fill
-              style={{ objectFit: "cover", borderRadius: "12px" }}
-            />
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+    const data = await res.json();
+    const movies: Film[] = data.Film.Film;
+
+    // renderizza la pagina con i dati
+    return (
+      <div
+        className="grid gap-4 p-5 justify-center"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          maxWidth: "100%",
+          margin: "0 auto",
+        }}
+      >
+        {movies.map((movie) => (
+          <Link key={movie._id} href={`/movie/${movie.id_tmdb}`}>
+            <div
+              className="rounded-2xl relative"
+              style={{ height: "300px", width: "200px" }}
+            >
+              <Image
+                src={`http://192.168.1.221:8000/poster/${movie.poster_path}`}
+                alt={movie.title}
+                fill
+                style={{ objectFit: "cover", borderRadius: "12px" }}
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
