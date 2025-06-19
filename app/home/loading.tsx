13dbd3471f814b4bee19b0e600581fefba6062f2
moreee-sa <morenolms41@gmail.com>
2025-06-19 16:45:57 +0200

    Rimosso useEffect nel loading.tsx

--- app/home/loading.tsx
+++ app/home/loading.tsx
@@ -1,19 +1,7 @@
 'use client'
 
-import { useEffect } from "react";
-
 export default function Loading() {
   const n: number = 20;
-
-  useEffect(() => {
-    const originalStyle = window.getComputedStyle(document.body).overflow;
-    document.body.style.overflow = "hidden";
-
-    return () => {
-      document.body.style.overflow = originalStyle;
-    };
-  }, []);
-
   return (
     <div
       className="grid gap-4 p-5 justify-center opacity-40"
@@ -23,12 +11,12 @@ export default function Loading() {
         maxWidth: "100%",
         margin: "0 auto",
       }}
-    >
+      >
       {Array.from({ length: n }).map((_, i) => (
         <div
-          key={i}
-          className="bg-amber-100 rounded-2xl animate-pulse"
-          style={{ height: "300px", width: "100%" }}
+        key={i}
+        className="bg-amber-100 rounded-2xl animate-pulse"
+        style={{ height: "300px", width: "100%" }}
         ></div>
       ))}
     </div>
