255ae862ec307a8983d52e3647858ba0575c4684
moreee-sa <morenolms41@gmail.com>
2025-06-21 20:51:47 +0200

    Aggiornata la pagina per accettere parametri slug e passarli come prop al componente lato client

--- app/movie/[slug]/page.tsx
+++ app/movie/[slug]/page.tsx
@@ -1,13 +1,41 @@
+import MoviePage from "@/components/movie/MoviePage";
+
+type Film = {
+  _id: string;
+  id_tmdb: number;
+  title: string;
+  overview: string;
+  backdrop_path: string;
+};
+
 export default async function MovieSlug({
   params,
 }: {
-  params: Promise<{ slug: number }>
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params
- 
-  return (
-    <div>
-      <h1>{slug}</h1>
-    </div>
-  )
+  const { slug } = await params;
+  const id_tmdb = Number(slug);
+
+  try {
+    const res = await fetch(`http://192.168.1.221:8000/movie/${slug}`);
+
+    if (!res.ok) {
+      console.error("Errore nella risposta:", res.status);
+      // not-found necessario
+    }
+
+    const movie: Film = await res.json();
+
+    return (
+      <MoviePage
+        id_tmdb={movie.id_tmdb}
+        title={movie.title}
+        overview={movie.overview}
+        backdrop_path={movie.backdrop_path}
+      />
+    );
+  } catch (error) {
+    console.error("Errore di rete o parsing:", error);
+    return <div>Errore nel caricamento</div>;
+  }
 }
