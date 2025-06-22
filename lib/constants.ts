2edb408aa61ba07ec28097f049e9d303fb6db3ba
moreee-sa <morenolms41@gmail.com>
2025-06-22 21:50:14 +0200

    Aggiunte nuove costanti per gli stili

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,11 @@
-export const MOBILEBREAKPOINT: string = "767px"
+// Informazioni del sito
 export const TITLE: string = "NextCatflix"
 export const VERSIONE: number = 0.1
+
+// Impostazioni API
 export const LOADINGCARD: number = 50
 export const APIURL: string = "http://0.0.0.0:8000"
+
+// Style
+export const MOBILEBREAKPOINT: string = "767px"
+export const DESKTOP_PADDING: string = "0 50px"
