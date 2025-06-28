b581453d0c7cb55e3356000d24f6fd53aac0eeb7
moreee-sa <morenolms41@gmail.com>
2025-06-29 00:31:28 +0200

    Aggiunta una nuova costante per il limite di parole per la descrizione

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,10 @@
 // Informazioni del sito
-export const TITLE: string = "NextCatflix"
-export const VERSIONE: number = 0.1
+export const TITLE: string = "NextCatflix";
+export const VERSIONE: number = 0.1;
 
 // Impostazioni API
-export const LOADINGCARD: number = 50
-export const APIURL: string = "http://0.0.0.0:8000"
+export const LOADINGCARD: number = 50;
+export const APIURL: string = "http://0.0.0.0:8000";
 
 // Screen Breakpoint
 export const MOBILEBREAKPOINT: string = "767px";
@@ -14,4 +14,7 @@ export const DESKTOPBREAKPOINT: string = "1440px"; // desktop normale
 export const FULLHDBREAKPOINT: string = "1920px"; // schermo intero su 1080p
 
 // Screen Padding
-export const DESKTOP_PADDING: string = "0 50px"
+export const DESKTOP_PADDING: string = "0 50px";
+
+// Dettagli film
+export const MAXDETAILS: number = 200;
