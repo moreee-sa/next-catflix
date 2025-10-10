8ab7d6d489b1957271dd272b129e2295c448e7a3
moreee-sa <morenolms41@gmail.com>
2025-10-10 23:05:09 +0200

    Aggiunto interface

--- app/watch/[slug]/WatchSlugClient.tsx
+++ app/watch/[slug]/WatchSlugClient.tsx
@@ -2,16 +2,9 @@
 
 import dynamic from "next/dynamic";
 
-const VideoPlayer = dynamic(
-  () => import("@/components/watch/VideoPlayer"),
-  { ssr: false }
-);
+const VideoPlayer = dynamic(() => import("@/components/watch/VideoPlayer"), { ssr: false });
 
-interface WatchSlugClientProps {
-  src: string;
-}
-
-export default function WatchSlugClient({ src }: WatchSlugClientProps) {
+export default function WatchSlugClient({ src }: { src: string }) {
   return (
     <div className="bg-black">
       <VideoPlayer src={src} autoplay />
