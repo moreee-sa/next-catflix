1e3792622398ed4427c782e49db833664fff0972
moreee-sa <morenolms41@gmail.com>
2025-10-10 23:21:14 +0200

    Fix tipizzazione params in WatchSlug che causava errore durante la compilazione

--- app/watch/[slug]/page.tsx
+++ app/watch/[slug]/page.tsx
@@ -2,19 +2,12 @@ import { APIURL } from "@/lib/constants";
 import WatchSlugClient from "./WatchSlugClient";
 import { notFound } from "next/navigation";
 
-interface WatchSlugParams {
-  params: { slug: string };
-}
-
-export default async function WatchSlug({ params }: WatchSlugParams) {
+export default async function WatchSlug({ params }: { params: Promise<{ slug: string }> }) {
   try {
     const { slug } = await params;
     const id_movie = slug;
-
-    const src = `${APIURL}/watch/${id_movie}`
-
+    const src = `${APIURL}/watch/${id_movie}`;
     return <WatchSlugClient src={src} />;
-
   } catch (error) {
     return notFound();
   }
