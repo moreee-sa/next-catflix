391bd30119409992d6d8aa6fa499a10cd5193b59
moreee-sa <morenolms41@gmail.com>
2025-11-15 00:20:30 +0100

    refactor: migliora la gestione del film in evidenza e semplifica il recupero dei dati dell'immagine

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,32 +11,37 @@ import '@fontsource-variable/montserrat';
 const featured: number = 1062722;
 
 export default async function HomePage() {
+  let featured_movie: MovieType | null = null;
+  let imageUrl: string = '';
+  let blurDataURL: string = '';
+
   try {
     // Featured Movie
     const res_featured = await fetch(`${APIURL}/movie/${featured}`);
     if (!res_featured || !res_featured.ok) notFound();
-    const featured_movie: MovieType = await res_featured.json();
+    featured_movie = await res_featured.json();
 
     try {
-      // Featured Movie BlurData
-      const imageUrl = `${APIURL}/backdrop/${featured_movie.backdrop_path}`;
-      const blurDataURL = await getBlurData(imageUrl);
+      if (!featured_movie) notFound();
+      imageUrl = `${APIURL}/backdrop/${featured_movie.backdrop_path}`;
+      blurDataURL = await getBlurData(imageUrl);
 
-      return (
-        <>
-          <FeaturedMovie movie={featured_movie} imagebackdrop={{ imageUrl, blurDataURL }} />
-          <div className="flex flex-col gap-5 my-5">
-            <SliderMovie endpoint="popular" titoloCategoria="Titoli recenti" />
-            {/* <SliderMovie endpoint="popular" titoloCategoria="Altri titoli" /> */}
-            {/* <SliderMovie endpoint="popular" titoloCategoria="Altri Film" /> */}
-          </div>
-          <InfoBanner />
-        </>
-      )
     } catch {
       notFound();
     }
   } catch {
     notFound();
   }
+
+  return (
+    <>
+      <FeaturedMovie movie={featured_movie} imagebackdrop={{ imageUrl, blurDataURL }} />
+      <div className="flex flex-col gap-5 my-5">
+        <SliderMovie endpoint="popular" titoloCategoria="Titoli recenti" />
+        {/* <SliderMovie endpoint="popular" titoloCategoria="Altri titoli" /> */}
+        {/* <SliderMovie endpoint="popular" titoloCategoria="Altri Film" /> */}
+      </div>
+      <InfoBanner />
+    </>
+  )
 }
